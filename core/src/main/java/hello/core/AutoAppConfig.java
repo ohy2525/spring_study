761a57c273e9b860454f56fcc1baa36e420fe4b4
ohy2525 <dhgodbs2@naver.com>
2022-10-24 09:50:01 +0900

    컴포넌트 스캔과 의존관계 자동 주입
    @Component @Autowired

---
+++ core/src/main/java/hello/core/AutoAppConfig.java
@@ -0,0 +1,13 @@
+package hello.core;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.FilterType;
+
+@Configuration
+@ComponentScan(
+        excludeFilters = @ComponentScan.Filter(type = FilterType.ANNOTATION, classes = Configuration.class)  // 자동 등록 제외(AppConfig 등록 방지)
+)
+public class AutoAppConfig {
+
+}
