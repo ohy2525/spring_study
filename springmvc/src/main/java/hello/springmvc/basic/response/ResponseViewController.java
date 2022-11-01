4b76a7ecc76f88b02de6592360383ead804078fe
ohy2525 <dhgodbs2@naver.com>
2022-11-01 10:26:29 +0900

    HTTP 응답 - 정적 리소스, 뷰  템플릿

---
+++ springmvc/src/main/java/hello/springmvc/basic/response/ResponseViewController.java
@@ -0,0 +1,28 @@
+package hello.springmvc.basic.response;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+public class ResponseViewController {
+
+    @RequestMapping("/response-view-v1")
+    public ModelAndView responseViewV1() {
+        ModelAndView mav = new ModelAndView("response/hello")
+                .addObject("data", "hello!");
+        return mav;
+    }
+
+    @RequestMapping("/response-view-v2")
+    public String responseViewV1(Model model) {
+        model.addAttribute("data", "hello!!!!");
+        return "response/hello";    //@Controller 에서 스트링을 반환하면 뷰의 논리적 이름이 된다.  @ResponseBody,@RestController 를 쓰면 문자 그대로 http 응답
+    }
+
+    @RequestMapping("/response/hello") //권장하지 않음
+    public void responseViewV3(Model model) {
+        model.addAttribute("data", "hello!!!!");
+    }
+}
