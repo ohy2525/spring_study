fb5ad28fa7e6421ffb3adffe9112dc09631b75bf
ohy2525 <dhgodbs2@naver.com>
2022-11-13 21:31:02 +0900

    서블릿 예외 처리

---
+++ exception/src/main/java/hello/exception/servlet/ServletInitializer.java
@@ -0,0 +1,14 @@
+package hello.exception.servlet;
+
+import hello.exception.ExceptionApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+public class ServletInitializer extends SpringBootServletInitializer {
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(ExceptionApplication.class);
+	}
+
+}
