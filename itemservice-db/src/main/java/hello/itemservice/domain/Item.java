7ceae5b47cc996b5c8e7757ecfd19f165af4be21
ohy2525 <dhgodbs2@naver.com>
2022-12-10 15:26:15 +0900

    JPA

--- itemservice-db/src/main/java/hello/itemservice/domain/Item.java
+++ itemservice-db/src/main/java/hello/itemservice/domain/Item.java
@@ -2,11 +2,16 @@ package hello.itemservice.domain;
 
 import lombok.Data;
 
+import javax.persistence.*;
+
 @Data
+@Entity  //테이블과 매핑되어 관리
 public class Item {
 
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY) //DB 에서 값을 증가시키는 전략
     private Long id;
 
+    @Column(name = "item_name", length = 10)  //없어도 자동으로 됨
     private String itemName;
     private Integer price;
     private Integer quantity;
