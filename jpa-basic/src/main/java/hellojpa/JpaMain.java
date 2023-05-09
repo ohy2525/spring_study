5a1bd86a93fb9d121f941f9d4d1545901da8f3d6
ohy2525 <dhgodbs2@naver.com>
2023-05-09 23:27:36 +0900

    JPQL 기본

--- jpa-basic/src/main/java/hellojpa/JpaMain.java
+++ jpa-basic/src/main/java/hellojpa/JpaMain.java
@@ -4,6 +4,7 @@ import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.EntityTransaction;
 import javax.persistence.Persistence;
+import java.util.List;
 
 public class JpaMain {
     public static void main(String[] args) {
@@ -15,13 +16,20 @@ public class JpaMain {
 
         try {
 
-            Member member1 = em.find(Member.class, 1L);
-            Member member = new Member();
-            member.setId(2L);
-            member.setName("HelloB");
+            Member findMember = em.find(Member.class, 1L);
+//            Member member = new Member();
+//            member.setId(2L);
+//            member.setName("HelloB");
+//
+//            em.persist(member); // 멤버 저장
+            //findMember.setName("helloJPA"); // 멤버 수정
 
-            em.persist(member); // 멤버 저장
+            List<Member> result = em.createQuery("select m from Member as m", Member.class)   // JPQL 페이징 할 때 편리
+                    .getResultList();
 
+            for (Member member : result) {
+                System.out.println("member.name = " + member.getName());
+            }
             tx.commit();
         } catch (Exception e) {
             tx.rollback();
